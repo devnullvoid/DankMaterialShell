4493b7c231863cfeeb59164516626d43637dbca7
Joaquim S. <rPlakama@proton.me>
2025-12-27 13:14:18 -0300

    matugen/template: Improve on generated theme + using dank16 instead (#1177)

--- quickshell/matugen/templates/neovim.lua
+++ quickshell/matugen/templates/neovim.lua
@@ -4,32 +4,39 @@ return {
 		priority = 1000,
 		config = function()
 			require('base16-colorscheme').setup({
-				base00 = '{{colors.background.default.hex}}',
-				base01 = '{{colors.surface_container.default.hex}}',
-				base02 = '{{colors.surface_container_highest.default.hex}}',
-				base03 = '{{colors.outline_variant.default.hex}}',
-				base04 = '{{colors.on_surface_variant.default.hex}}',
-				base05 = '{{colors.on_surface.default.hex}}',
-				base06 = '{{colors.on_surface.default.hex}}',
-				base07 = '{{colors.inverse_on_surface.default.hex}}',
-				base08 = '{{colors.error.default.hex}}',
-				base09 = '{{colors.tertiary.default.hex}}',
-				base0A = '{{colors.on_surface_variant.default.hex}}',
-				base0B = '{{colors.primary.default.hex}}',
-				base0C = '{{colors.secondary.default.hex}}',
-				base0D = '{{colors.primary.default.hex}}',
-				base0E = '{{colors.secondary.default.hex}}',
-				base0F = '{{colors.error.default.hex}}',
+				base00 = '{{dank16.color0.default.hex}}',
+				base01 = '{{dank16.color8.default.hex}}',
+				base02 = '{{dank16.color14.default.hex}}',
+				base03 = '{{dank16.color5.default.hex}}',
+				base04 = '{{dank16.color7.default.hex}}',
+				base05 = '{{dank16.color12.default.hex}}',
+				base06 = '{{dank16.color6.default.hex}}',
+				base07 = '{{dank16.color15.default.hex}}',
+
+				base08 = '{{dank16.color4.default.hex}}',
+				base09 = '{{dank16.color3.default.hex}}',
+				base0A = '{{dank16.color11.default.hex}}',
+				base0B = '{{dank16.color2.default.hex}}',
+				base0C = '{{dank16.color10.default.hex}}',
+				base0D = '{{dank16.color9.default.hex}}',
+				base0E = '{{dank16.color1.default.hex}}',
+				base0F = '{{dank16.color13.default.hex}}',
 			})
 
-			local function set_hl_mutliple(groups, value)
-				for _, v in pairs(groups) do vim.api.nvim_set_hl(0, v, value) end
-			end
+			vim.api.nvim_set_hl(0, 'Visual', {
+				bg = '{{dank16.color14.default.hex}}',
+				fg = '{{dank16.color15.default.hex}}',
+				bold = true
+			})
 
-			vim.api.nvim_set_hl(0, 'Visual',
-				{ bg = '{{colors.primary_container.default.hex}}', fg = '{{colors.on_primary_container.default.hex}}', bold = true })
-			vim.api.nvim_set_hl(0, 'LineNr', { fg = '{{colors.outline_variant.default.hex}}' })
-			vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '{{colors.primary.default.hex}}', bold = true })
+			vim.api.nvim_set_hl(0, 'LineNr', {
+				fg = '{{dank16.color8.default.hex}}'
+			})
+
+			vim.api.nvim_set_hl(0, 'CursorLineNr', {
+				fg = '{{dank16.color6.default.hex}}',
+				bold = true
+			})
 
 			local current_file_path = vim.fn.stdpath("config") .. "/lua/plugins/dankcolors.lua"
 
@@ -42,7 +49,7 @@ return {
 
 					if new_spec and new_spec[1] and new_spec[1].config then
 						new_spec[1].config()
-						print("󰂖 Matugen: Colors reloaded!")
+						print("Theme reload")
 					end
 				end))
 			end
