80060ee925ffcea6f5e759cbf79393a2f6e05f2d
purian23 <purian23@gmail.com>
2026-09-22 23:29:34 -0400

    fix(debian): obs preinstall & ID checks for debian testing builds

--- core/install.sh
+++ core/install.sh
@@ -8,7 +8,7 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 # Check for root privileges
-if [ "$(id -u)" == "0" ]; then
+if [ "$(id -u)" = "0" ]; then
     printf "%bError: This script must not be run as root%b\n" "$RED" "$NC"
     exit 1
 fi
